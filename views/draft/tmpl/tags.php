e1f23e008ad2ebebb806cc7f4e3efd6efe539f95
M. Drew LaMar <mdlama@wm.edu>
2019-05-07 14:38:12 -0400

    [style] Formatting fixes
    (cherry picked from commit 99dbffeba1efc862f0aaa27bd7336d3ebc9cb635)

--- views/draft/tmpl/tags.php
+++ views/draft/tmpl/tags.php
@@ -47,76 +47,81 @@ $this->css('tags.css')
 ?>
 
 <!-- Load content selection browser //-->
-<div id="<?php echo $elName; ?>" class="blockelement<?php echo $required ? ' el-required' : ' el-optional';
-echo $complete ? ' el-complete' : ' el-incomplete'; ?> <?php echo $curatorStatus->status == 1 ? ' el-passed' : ''; echo $curatorStatus->status == 0 ? ' el-failed' : ''; echo $curatorStatus->updated && $curatorStatus->status != 2 ? ' el-updated' : ''; ?>">
-			<div class="element_editing">
-				<div class="pane-wrapper">
-					<span class="checker">&nbsp;</span>
-					<label id="<?php echo $elName; ?>-lbl">
-						<?php if ($required) { ?><span class="required"><?php echo Lang::txt('PLG_PROJECTS_PUBLICATIONS_REQUIRED'); ?></span>
-						<?php } else { ?><span class="optional"><?php echo Lang::txt('PLG_PROJECTS_PUBLICATIONS_OPTIONAL'); ?></span><?php } ?>
-						<?php echo ucfirst(Lang::txt('PLG_PROJECTS_PUBLICATIONS_PUBLICATION_TAGS')); ?>
-					</label>
-						<?php echo $this->pub->_curationModel->drawCurationNotice($curatorStatus, $props, 'author', $elName); ?>
-
-					<fieldset class="focus-areas">
-						<?php
-							if (count($focusareas) > 0):
-								$fa_existing = $recommended->get_existing_focus_areas_map();
-								$fa_props = $recommended->get_focus_area_properties();
-
-								echo 'Choose from these recommended tags:';
-								$idx = 0;
-								foreach ($focusareas as $label=>$fas):
-								?>
-									<fieldset value="<?php echo ($fa_props[$label]['mandatory_depth'] ? $fa_props[$label]['mandatory_depth'] : 0) ?>">
-										<legend>
-											<span class="tooltips" title='<?php echo $fa_props[$label]['about']; ?>'><?php echo ($fa_props[$label]['label'] ? $fa_props[$label]['label'] : $label) ?></span>
-											<?php echo ($fa_props[$label]['mandatory_depth'] ? '<span class="required">required</span>' : '<span class="optional">optional</span>'); ?>
-										</legend>
-										<?php $recommended->fa_controls(++$idx, $fas, $fa_props, $fa_existing); ?>
-									</fieldset>
-								<?php
-								endforeach;
-							endif;
-						?>
-					</fieldset>
-						<?php
-						$tf = Event::trigger( 'hubzero.onGetMultiEntry', array(array('tags', 'tags', 'actags', '', $recommended->get_existing_tags_value_list())) );
-
-						echo 'Enter your own tags below:';
-						if (count($tf) > 0) {
-							echo $tf[0];
-						} else {
-							echo '<textarea name="tags" id="tags" rows="6" cols="35">' . $recommended->get_existing_tags_value_list() . '</textarea>' . "\n";
-						}
-						?>
-				</div>
-			</div>
-</div>
-<?php if ($this->categories && count($this->categories) > 1) { ?>
-<div class="blockelement el-optional el-complete">
+<div id="<?php echo $elName; ?>" class="blockelement<?php
+	echo $required ? ' el-required' : ' el-optional';
+	echo $complete ? ' el-complete' : ' el-incomplete';
+	echo $curatorStatus->status == 1 ? ' el-passed' : '';
+	echo $curatorStatus->status == 0 ? ' el-failed' : '';
+	echo $curatorStatus->updated && $curatorStatus->status != 2 ? ' el-updated' : '';
+	?>">
 	<div class="element_editing">
 		<div class="pane-wrapper">
-				<span class="checker">&nbsp;</span>
-				<label><?php echo Lang::txt('PLG_PROJECTS_PUBLICATIONS_CONTENT_SEARCH_CATEGORY'); ?></label>
-		<?php foreach ($this->categories as $cat)
-		{
-			$params = new \Hubzero\Config\Registry($cat->params);
+			<span class="checker">&nbsp;</span>
+			<label id="<?php echo $elName; ?>-lbl">
+				<?php if ($required) { ?><span class="required"><?php echo Lang::txt('PLG_PROJECTS_PUBLICATIONS_REQUIRED'); ?></span>
+				<?php } else { ?><span class="optional"><?php echo Lang::txt('PLG_PROJECTS_PUBLICATIONS_OPTIONAL'); ?></span><?php } ?>
+				<?php echo ucfirst(Lang::txt('PLG_PROJECTS_PUBLICATIONS_PUBLICATION_TAGS')); ?>
+			</label>
+		  <?php echo $this->pub->_curationModel->drawCurationNotice($curatorStatus, $props, 'author', $elName); ?>
 
-			// Skip inaplicable category
-			if (!$params->get('type_' . $this->pub->base, 1))
-			{
-				continue;
+			<fieldset class="focus-areas">
+				<?php
+					if (count($focusareas) > 0):
+						$fa_existing = $recommended->get_existing_focus_areas_map();
+						$fa_props = $recommended->get_focus_area_properties();
+
+						echo 'Choose from these recommended tags:';
+						$idx = 0;
+						foreach ($focusareas as $label=>$fas):
+				?>
+							<fieldset value="<?php echo ($fa_props[$label]['mandatory_depth'] ? $fa_props[$label]['mandatory_depth'] : 0) ?>">
+								<legend>
+									<span class="tooltips" title='<?php echo $fa_props[$label]['about']; ?>'><?php echo ($fa_props[$label]['label'] ? $fa_props[$label]['label'] : $label) ?></span>
+									<?php echo ($fa_props[$label]['mandatory_depth'] ? '<span class="required">required</span>' : '<span class="optional">optional</span>'); ?>
+								</legend>
+								<?php $recommended->fa_controls(++$idx, $fas, $fa_props, $fa_existing); ?>
+							</fieldset>
+						<?php
+						endforeach;
+					endif;
+				?>
+			</fieldset>
+			<?php
+			$tf = Event::trigger( 'hubzero.onGetMultiEntry', array(array('tags', 'tags', 'actags', '', $recommended->get_existing_tags_value_list())) );
+
+			echo 'Enter your own tags below:';
+			if (count($tf) > 0) {
+				echo $tf[0];
+			} else {
+				echo '<textarea name="tags" id="tags" rows="6" cols="35">' . $recommended->get_existing_tags_value_list() . '</textarea>' . "\n";
 			}
 			?>
-				<div class="pubtype-block">
-					<input type="radio" name="pubtype" value="<?php echo $cat->id; ?>" <?php if ($this->pub->category == $cat->id) { echo 'checked="checked"'; } ?> class="radio" />
-					<?php echo $cat->name; ?>
-					<span><?php echo $cat->description; ?></span>
-				</div>
-		<?php } ?>
 		</div>
 	</div>
 </div>
-<?php } ?>
+<?php if ($this->categories && count($this->categories) > 1) { ?>
+	<div class="blockelement el-optional el-complete">
+		<div class="element_editing">
+			<div class="pane-wrapper">
+					<span class="checker">&nbsp;</span>
+					<label><?php echo Lang::txt('PLG_PROJECTS_PUBLICATIONS_CONTENT_SEARCH_CATEGORY'); ?></label>
+			<?php foreach ($this->categories as $cat)
+			{
+				$params = new \Hubzero\Config\Registry($cat->params);
+
+				// Skip inaplicable category
+				if (!$params->get('type_' . $this->pub->base, 1))
+				{
+					continue;
+				}
+				?>
+					<div class="pubtype-block">
+						<input type="radio" name="pubtype" value="<?php echo $cat->id; ?>" <?php if ($this->pub->category == $cat->id) { echo 'checked="checked"'; } ?> class="radio" />
+						<?php echo $cat->name; ?>
+						<span><?php echo $cat->description; ?></span>
+					</div>
+			<?php } ?>
+			</div>
+		</div>
+	</div>
+<?php }
