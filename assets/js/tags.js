2adf7383f27c3279daa8667636b9e8f0a80eba6a
M. Drew LaMar <mdlama@wm.edu>
2021-02-18 11:19:39 -0500

    update copyright/trademark ownership
    (cherry picked from commit 7a772265691cebdda2695389145abe67bd7bcd96)

--- assets/js/tags.js
+++ assets/js/tags.js
@@ -1,3 +1,9 @@
+/**
+ * @package    hubzero-cms
+ * @copyright  Copyright 2005-2019 The Regents of the University of California.
+ * @license    http://opensource.org/licenses/MIT MIT
+ */
+
 jQuery(function($) {
 	$('.js-only').css('display', 'inline');
 	var nested_fa = $('.fa .fa');
