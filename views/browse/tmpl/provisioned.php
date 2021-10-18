9ce1b7a326eff9088fe2da9a2a65a79a36aad21f
M. Drew LaMar <mdlama@wm.edu>
2021-10-18 12:50:09 -0400

    [fix] Link to author info
    (cherry picked from commit 61f304a436e347883ac5b8fd2647f0168446acc6)

--- views/browse/tmpl/provisioned.php
+++ views/browse/tmpl/provisioned.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package    hubzero-cms
  * @copyright  Copyright 2005-2019 HUBzero Foundation, LLC.
@@ -11,9 +12,8 @@ defined('_HZEXEC_') or die();
 $pubconfig = Component::params('com_publications');
 
 $submit_url = Route::url('index.php?option=com_publications&task=submit&action=choose');
-if (User::isGuest())
-{
-  // Could be a problem here - imagining possible issue with this
+if (User::isGuest()) {
+	// Could be a problem here - imagining possible issue with this
 	$submit_url = Route::url('index.php?option=com_users&view=login&return=' . base64_encode($submit_url));
 }
 ?>
@@ -25,20 +25,20 @@ if (User::isGuest())
 				<div class="software-instructions">
 					<h4>Got a Shiny App?</h4>
 					<p>Please read these <a href="https://docs.google.com/document/d/1TtRbOhlUfkD2a-Ii35py-55mlYsNhHzyexSJ9FsA70M/edit?usp=sharing" target="_blank">special instructions</a>. For other types of software, <span class="helpme"><a href="/support/">contact us</a></span> for help.</p>
-			</div>
+				</div>
 
-			<div class="kb-links">
-				<h5>Related Articles</h5>
-				<ul>
-					<li>[Article] <a href="/kb/resources/postresource">Using Publications to Post Resources on QUBES</a></li>
-					<li>[Article] <a href="/authorinfo">Information for Authors and Submitters</a></li>
-				</ul>
-			</div>
+				<div class="kb-links">
+					<h5>Related Articles</h5>
+					<ul>
+						<li>[Article] <a href="/kb/resources/postresource">Using Publications to Post Resources on QUBES</a></li>
+						<li>[Article] <a href="/kb/resources/authorinfo">Information for Authors and Submitters</a></li>
+					</ul>
+				</div>
 		</aside>
 
 		<div class="submit-a-resource-wrap">
 			<h3>Quick and Easy. Get started today!</h3>
-			<a id="submit-resource" href="<?php echo $submit_url;?>" class="btn submit-resource-btn">Submit a Resource</a>
+			<a id="submit-resource" href="<?php echo $submit_url; ?>" class="btn submit-resource-btn">Submit a Resource</a>
 		</div>
 
 		<div class="submit-partner-resource-wrap">
@@ -56,15 +56,12 @@ if (User::isGuest())
 		</div>
 	</div>
 	<div>
-		<?php if (User::isGuest())
-		{
+		<?php if (User::isGuest()) {
 			$this->view('intro')
-			     ->set('project', $this->project)
-			     ->set('pub', $this->pub)
-			     ->display();
-		}
-		else
-		{
+				->set('project', $this->project)
+				->set('pub', $this->pub)
+				->display();
+		} else {
 			$filters = array();
 
 			// Get user projects
@@ -77,37 +74,36 @@ if (User::isGuest())
 			$filters['start'] 	= Request::getInt('limitstart', 0);
 
 			// Get publications created by user
-			$mypubs = $this->pub->entries( 'list', $filters );
-			$total  = $this->pub->entries( 'count', $filters );
-			?>
-			<form action="<?php echo Route::url('index.php?option=com_publications&task=submit'); ?>" method="post" id="browseForm" >
-			<h3><?php echo Lang::txt('PLG_PROJECTS_PUBLICATIONS_MY_PUBLICATIONS'); ?></h3>
-			<?php if (!empty($mypubs)) { ?>
-			<ul class="mypubs">
-				<?php
+			$mypubs = $this->pub->entries('list', $filters);
+			$total  = $this->pub->entries('count', $filters);
+		?>
+			<form action="<?php echo Route::url('index.php?option=com_publications&task=submit'); ?>" method="post" id="browseForm">
+				<h3><?php echo Lang::txt('PLG_PROJECTS_PUBLICATIONS_MY_PUBLICATIONS'); ?></h3>
+				<?php if (!empty($mypubs)) { ?>
+					<ul class="mypubs">
+						<?php
 
-				foreach ($mypubs as $row)
-				{
-					$this->view('_item')
-					     ->set('row', $row)
-					     ->set('project', $this->project)
-					     ->set('pub', $this->pub)
-					     ->display();
-				}
-				?>
-			</ul>
-			<?php // Pagination
-			$pageNav = new \Hubzero\Pagination\Paginator(
-				$total,
-				$filters['start'],
-				$filters['limit']
-			);
-			echo $pageNav->render();
-			?>
+						foreach ($mypubs as $row) {
+							$this->view('_item')
+								->set('row', $row)
+								->set('project', $this->project)
+								->set('pub', $this->pub)
+								->display();
+						}
+						?>
+					</ul>
+					<?php // Pagination
+					$pageNav = new \Hubzero\Pagination\Paginator(
+						$total,
+						$filters['start'],
+						$filters['limit']
+					);
+					echo $pageNav->render();
+					?>
 			</form>
-			<?php } else { ?>
-				<p class="noresults"><?php echo Lang::txt('PLG_PROJECTS_PUBLICATIONS_NO_RELEVANT_PUBS_FOUND'); ?></p>
-			<?php } ?>
+		<?php } else { ?>
+			<p class="noresults"><?php echo Lang::txt('PLG_PROJECTS_PUBLICATIONS_NO_RELEVANT_PUBS_FOUND'); ?></p>
 		<?php } ?>
+	<?php } ?>
 	</div>
 </div>
