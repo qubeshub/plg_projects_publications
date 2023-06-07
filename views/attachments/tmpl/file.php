889ebb56d800b764712380f19ac09e34a9e05540
M. Drew LaMar <mdlama@wm.edu>
2023-06-07 14:02:54 -0400

    [fix] Pub attachments always downloadable
    Previously, the code was checking to see if file existed in project
    space before showing download icon. However, the attachment was
    already copied to publication directory, so still accessible. The
    check has therefore been removed so file is always accessible from
    publication directory.
    
    (cherry picked from commit 7cb25448f9a09f4701a0381871bad7d89e2c3fe3)

--- views/attachments/tmpl/file.php
+++ views/attachments/tmpl/file.php
@@ -25,13 +25,11 @@ if ($data->get('viewer') != 'freeze')
 				<?php if ($data->total > 1) { ?>
 				<a href="#" class="item-reorder handle" title="<?php echo Lang::txt('PLG_PROJECTS_PUBLICATIONS_REORDER'); ?>">&nbsp;</a>
 				<?php } ?>
-				<?php if ($data->exists()) { ?>
 				<a href="<?php echo $data->get('downloadUrl'); ?>" class="item-download" title="<?php echo Lang::txt('PLG_PROJECTS_PUBLICATIONS_DOWNLOAD'); ?>">&nbsp;</a>
-				<?php } ?>
 				<a href="<?php echo Route::url($pub->link('editversion') . '&action=edititem&aid=' . $data->get('id') . '&p=' . $data->get('props')); ?>" class="showinbox item-edit" title="<?php echo Lang::txt('PLG_PROJECTS_PUBLICATIONS_RELABEL'); ?>">&nbsp;</a>
 				<a href="<?php echo Route::url($pub->link('editversion') . '&action=deleteitem&aid=' . $data->get('id') . '&p=' . $data->get('props')); ?>" class="item-remove" title="<?php echo Lang::txt('PLG_PROJECTS_PUBLICATIONS_REMOVE'); ?>">&nbsp;</a>
 			</span>
-			<?php } elseif ($data->exists()) { ?>
+			<?php } else { ?>
 				<span><a href="<?php echo $data->get('downloadUrl'); ?>" class="item-download" title="<?php echo Lang::txt('PLG_PROJECTS_PUBLICATIONS_DOWNLOAD'); ?>">&nbsp;</a></span>
 			<?php } ?>
 		</span>
